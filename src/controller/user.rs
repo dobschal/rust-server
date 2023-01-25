e78e493c9cdb0f238f6f99d6c10fc4cb11fac001
Sascha Dobschal <s.dobschal@tdsoftware.de>
2023-01-25 14:38:12 +0100

    add error handling for request controllers

--- src/controller/user.rs
+++ src/controller/user.rs
@@ -1,18 +1,25 @@
 use crate::{
     database::{self},
     dto::UserDto,
-    util::respond_with_success,
+    util::{respond_with_bad_request_error, respond_with_error, respond_with_success},
     Request,
 };
-use serde_json::{from_str, json};
+use serde_json::{from_str, json, Error};
 use std::{io::Write, net::TcpStream};
 
-// TODO: instead of using expect, handle with Result
-
 pub fn add_user(request: &mut Request, stream: &mut TcpStream) {
-    let user_dto: UserDto =
-        from_str(&request.payload).expect("Could not parse requst payload to UserDto");
-    database::add_user(user_dto);
+    let parse_user_result: Result<UserDto, Error> = from_str::<UserDto>(&request.payload);
+    if parse_user_result.is_err() {
+        return respond_with_bad_request_error(stream, "Please request with valid user data.");
+    }
+    let add_user_result = database::add_user(parse_user_result.unwrap());
+    if add_user_result.is_err() {
+        return respond_with_error(
+            stream,
+            "Could not insert user.",
+            "500 Internal Server Error",
+        );
+    }
     respond_with_success(stream);
 }
 
@@ -23,8 +30,11 @@ pub fn fake(_: &mut Request, stream: &mut TcpStream) {
 
 pub fn get_users(_request: &mut Request, stream: &mut TcpStream) {
     let status_line = "HTTP/1.1 200 OK";
-    let users = database::get_users();
-    let contents = json!(users).to_string();
+    let get_users_result = database::get_users();
+    if get_users_result.is_err() {
+        return respond_with_error(stream, "Could not get users", "500 Internal Server Error");
+    }
+    let contents = json!(get_users_result.unwrap()).to_string();
     let length = contents.len();
     let content_type = "application/json";
     let response = format!(
--- src/database/user.rs
+++ src/database/user.rs
@@ -2,21 +2,13 @@ use mysql::{params, prelude::*};
 
 use crate::{database::get_connection, dto::UserDto, entity::User};
 
-pub fn get_users() -> Vec<User> {
-    println!("Select users from database...");
-    let users = get_connection()
-        .query_map("SELECT id, name from user", |(id, name)| User { id, name })
-        .expect("Could not get users...");
-    println!("Users: {:?}", users);
-    return users;
+pub fn get_users() -> Result<Vec<User>, mysql::Error> {
+    get_connection().query_map("SELECT id, name from user", |(id, name)| User { id, name })
 }
 
-pub fn add_user(user: UserDto) {
-    println!("Add user to database...");
-    get_connection()
-        .exec_drop(
-            "INSERT INTO user (name) VALUES (:name)",
-            params! { "name" => user.name },
-        )
-        .expect("Could not insert user...");
+pub fn add_user(user: UserDto) -> Result<(), mysql::Error> {
+    get_connection().exec_drop(
+        "INSERT INTO user (name) VALUES (:name)",
+        params! { "name" => user.name },
+    )
 }
